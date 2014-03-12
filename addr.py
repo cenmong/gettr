39f1abeff522519e509332cd798e50f8077e8195
cenmong <cenmong@gmail.com>
2014-03-12 19:28:39 +0800

    add more than 10000 random addresses in addr. each prefix(>32,<64) has one address

--- addr.py
+++ addr.py
@@ -1,3 +1,25 @@
-dest = ['2001:da8:1:100::80',
-     '2001:4641::1',
-     '2001:1430:a:50::50',]
+# -*- coding:utf-8 -*-
+import random
+from netaddr import *
+
+f = open('ribascii', 'r')
+pfxs = []
+dest = []
+
+for line in f.readlines():
+    if line.split(': ')[0] == '|ANNOUNCE':
+        pfx = line.split(': ')[1].split('/')[0] 
+        leng = int(line.split(': ')[1].split('/')[1])
+        if leng < 32 or leng > 64:
+            continue
+        
+        if pfx in pfxs:#same prefix appear again
+            continue
+        pfxs.append(pfx)
+
+        random.seed()
+        ip6 = IPAddress(pfx) + random.getrandbits(128-leng)
+        dest.append(ip6)
+
+f.close()
+
